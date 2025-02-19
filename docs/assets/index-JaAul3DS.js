4bb01bb4e53b7b3cfe6eff7358428f00561da6a2
Nathan Harris <nharrisanalyst@gmail.com>
2025-02-18 18:56:52 -0800

    update build

--- docs/assets/index-JaAul3DS.js
+++ docs/assets/index-JaAul3DS.js
@@ -7,7 +7,7 @@
   <div>
     <div id="title">
     <div id="mainTitle">
-    Patrick Mahomes and The Kansas City Chiefs Dug Themselves a Historic Unsurmountable Half Time Deficit.
+    Patrick Mahomes and The Kansas City Chiefs Dug Themselves a Historic Insurmountable Half Time Deficit.
     </div>
     <div id="subTitle">
      The half time score is very predictive of who will win the game. Before Super Bowl 51 with Tom Bardy's New England Patriots no team had ever overcome a 7 point half time deficit.
